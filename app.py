27160e70415da80ee0e4b7cc7aa92a11c65fe256
htadmg <agatadominguestec@hotmail.com>
2024-09-24 16:47:05 -0300

    insirindo o arquivo requiremensts.txt, flake8 e organizacao do codigo para melhores praticas

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ PROJETOS_CURSOS = {
     "Análise de Dados e Previsão de Sobrevivência": "https://github.com/htadmg/dataset_titanic",
     "Dashboard de Vendas": "https://github.com/htadmg/projeto_streamilt_dash",
     "Dashboard de Vendas com Dash e Flask": "https://github.com/htadmg/dashboard_com_dash",
-    "Lista de Tarefas com Django": "https://github.com/htadmg/lista_de_tarefas",    
+    "Lista de Tarefas com Django": "https://github.com/htadmg/lista_de_tarefas",
 }
 
 st.set_page_config(
@@ -88,7 +88,7 @@ st.write(
 st.subheader("Skills")
 st.write(
     """
-    - Programação: Python | C# | JavaScript 
+    - Programação: Python | C# | JavaScript
     - Desenvolvimento Web: Flask | Streamlit | Dash | HTML | CSS
     - Banco de Dados: SQL | PostgreSql
     - Ferramentas: Git | Visual Studio | PyCharm
@@ -103,11 +103,11 @@ st.write("**Desenvolvedor de Sistemas | Conecta Marketplaces**")
 st.write("05/2024 - o momento")
 st.write(
     """
-        Atualmente, estou trabalhando em um projeto de integração com o Mercado Livre, 
-        onde desenvolvemos uma solução robusta utilizando a API do Mercado Livre. 
-        O projeto abrange várias funcionalidades, incluindo o gerenciamento de faturamento, 
-        busca e acompanhamento de pedidos, consulta e atualização de preços de anúncios, 
-        além de monitoramento da manutenção de anúncios. Implementamos um sistema de login e autenticação de usuários, 
+        Atualmente, estou trabalhando em um projeto de integração com o Mercado Livre,
+        onde desenvolvemos uma solução robusta utilizando a API do Mercado Livre.
+        O projeto abrange várias funcionalidades, incluindo o gerenciamento de faturamento,
+        busca e acompanhamento de pedidos, consulta e atualização de preços de anúncios,
+        além de monitoramento da manutenção de anúncios. Implementamos um sistema de login e autenticação de usuários,
         garantindo segurança e eficiência, utilizando Flask e Python como base para a arquitetura do sistema.
     """
 )
@@ -116,15 +116,15 @@ st.write("**Desenvolvedor de Sistemas | Techmillen Consultoria e Projetos**")
 st.write("06/2023 - 05/2024")
 st.write(
     """
-        Trabalhei no desenvolvimento de uma aplicação para o gerenciamento de salões de beleza utilizando C# e .NET, 
-        com interface responsiva construída com Bootstrap e interatividade aprimorada com jQuery. 
-        A aplicação permitia o controle de agendamentos, cadastro de clientes, 
-        gerenciamento de funcionários e serviços, oferecendo uma solução eficiente 
+        Trabalhei no desenvolvimento de uma aplicação para o gerenciamento de salões de beleza utilizando C# e .NET,
+        com interface responsiva construída com Bootstrap e interatividade aprimorada com jQuery.
+        A aplicação permitia o controle de agendamentos, cadastro de clientes,
+        gerenciamento de funcionários e serviços, oferecendo uma solução eficiente
         para otimizar as operações do negócio.
     """
 )
 
-# cursos # 
+# cursos #
 st.subheader("Projetos")
 for curso, link in PROJETOS_CURSOS.items():
     st.write(f"[{curso}]({link})")
